4b7e69bda56dec8f0b5d090583146ceb8088a80b
Leejaemin34 <128348214+Leejaemin34@users.noreply.github.com>
2023-04-05 14:01:46 +0900

    Update volume_clac.py
    교수님 피드백 반영 및 택배요금 계산코드 추가

--- volume_clac.py
+++ volume_clac.py
@@ -1,7 +1,24 @@
-가로 = float(input("가로:"))
-세로 = float(input("세로:"))
-높이 = float(input("높이:"))
+#상자의 입력값
+width  = float(input("Width:"))
+length = float(input("Length:"))
+heigth = float(input("Heigth:"))
+#부피계산
+volume = width * length * heigth
 
-부피 = 세로 * 가로 * 높이
+print("Volume:", volume, "cm^3")
+#총길이계산
+total_length = width + length + heigth
+#총길이 별 택배요금
+if    total_length <= 80:
+      charge = 5000
+elif  total_length <= 100:
+      charge = 8000
+elif  total_length <= 120:
+      charge = 10000
+elif  total_length <= 160:
+      charge = 13000
+else  :
+      charge = "unavailable"
 
-print("박스의 부피:", 부피)
+print("Total length:", total_length)
+print("Charge:", charge)
